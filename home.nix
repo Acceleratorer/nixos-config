1a7cf08073a218e670ee95e5293c9b35fd2bcf61
Acceleratorer <noobtan77@gmail.com>
2026-08-06 17:24:21 +0700

    feat(desktop): apply native visual consistency

--- home.nix
+++ home.nix
@@ -1,10 +1,43 @@
-{ caelestia-shell, ... }:
+{ caelestia-shell, pkgs, ... }:
 
 {
   imports = [
     caelestia-shell.homeManagerModules.default
     ./desktop/profiles.nix
   ];
+
+  gtk = {
+    enable = true;
+    colorScheme = "dark";
+
+    font = {
+      name = "Adwaita Sans";
+      size = 11;
+    };
+
+    iconTheme = {
+      package = pkgs.papirus-icon-theme;
+      name = "Papirus-Dark";
+    };
+  };
+
+  home.pointerCursor = {
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Ice";
+    size = 24;
+    gtk.enable = true;
+    x11.enable = true;
+  };
+
+  qt = {
+    enable = true;
+    platformTheme.name = "gtk3";
+  };
+
+  systemd.user.sessionVariables = {
+    XCURSOR_THEME = "Bibata-Modern-Ice";
+    XCURSOR_SIZE = "24";
+  };
 
   # Change only this value, rebuild, then begin a new Hyprland session.
   nixosCryoforge.desktopProfile = "classic";
